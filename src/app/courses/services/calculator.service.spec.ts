a71673f9d16e325ffd315851a0769574aa4f08d9
Tarun Chopra <tchopra@catalystone.com>
2020-07-11 00:03:31 +0530

    Implemented service unit test-cases

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,39 @@
+import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
+
+describe('CalculatorService', () => {
+
+    let loggerSpy: any;
+    let calculator: CalculatorService;
+
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {
+                    provide: LoggerService,
+                    useValue: loggerSpy
+                }
+            ]
+        });
+        calculator = TestBed.inject(CalculatorService);
+    });
+
+    it('should add two numbers', () => {
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers', () => {
+        const result = calculator.subtract(2, 2);
+
+        expect(result).toBe(0, 'Unexpected substraction result');
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+});
